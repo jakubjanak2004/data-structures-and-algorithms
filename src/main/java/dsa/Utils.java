081ebe34096583d7c38d9a03fb1f73d62a94ea86
Jakub Janak <janakja5@fel.cvut.cz>
2025-04-14 18:52:10 +0200

    swap made public

--- src/main/java/dsa/Utils.java
+++ src/main/java/dsa/Utils.java
@@ -1,7 +1,7 @@
 package dsa;
 
 public class Utils {
-    static void swap(int[] array, int i1, int i2) {
+    static public void swap(int[] array, int i1, int i2) {
         int temp = array[i1];
         array[i1] = array[i2];
         array[i2] = temp;
