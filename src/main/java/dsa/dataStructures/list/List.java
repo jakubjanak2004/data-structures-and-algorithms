78f2ef20ba0d241f88e6489436a90676d54d38f9
Jakub Janak <janakja5@fel.cvut.cz>
2025-04-26 09:03:21 +0200

    List interface added

--- src/main/java/dsa/dataStructures/list/List.java
+++ src/main/java/dsa/dataStructures/list/List.java
@@ -1,4 +1,14 @@
 package dsa.dataStructures.list;
 
-public class List {
+public interface List<T> {
+    int length();
+    boolean empty();
+    boolean full();
+    T read();
+    void insert(T t);
+    void delete();
+    void first();
+    void last();
+    void next();
+    void prev();
 }
