c975f3bf09d20acfd1ee65e4091caf069a19f352
Jakub Janak <janakja5@fel.cvut.cz>
2025-04-26 18:56:31 +0200

    added linked lists and changed the structure of the project

---
+++ src/main/java/dsa/dataStructures/list/DoublyLinkedList.java
@@ -0,0 +1,107 @@
+package dsa.dataStructures.list;
+
+public class DoublyLinkedList<T> implements List<T> {
+    ListNode<T> head;
+    ListNode<T> tail;
+    ListNode<T> point;
+    int len;
+
+    @Override
+    public int length() {
+        return len;
+    }
+
+    @Override
+    public boolean empty() {
+        return head == null;
+    }
+
+    @Override
+    public T read() {
+        if (point != null) {
+            return point.getValue();
+        }
+        return null;
+    }
+
+    @Override
+    public void insert(T t) {
+        ListNode<T> help = new ListNode<>();
+        if (point == null) {
+            help.setValue(t);
+            if (tail == null) {
+                head = help;
+            } else {
+                help.setPrevNode(tail);
+                tail.setNextNode(help);
+            }
+            tail = help;
+        } else {
+            // setting the prev and next node
+            help.setValue(point.getValue());
+            help.setNextNode(point.getNextNode());
+            point.getNextNode().setPrevNode(help);
+            // setting the prev and next node
+            point.setValue(t);
+            point.setNextNode(help);
+            help.setPrevNode(point);
+            point = help;
+        }
+        len++;
+    }
+
+    @Override
+    public void delete() {
+        ListNode<T> help;
+        if (point != null) {
+            help = point.getNextNode();
+            if (head == point) {
+                head = help;
+            }
+            if (tail == point) {
+                tail = tail.getPrevNode();
+            }
+            if (help != null) {
+                help.setPrevNode(point.getPrevNode());
+            }
+            if (point.getPrevNode() != null) {
+                point.getPrevNode().setNextNode(help);
+            }
+            point = help;
+            len--;
+        }
+    }
+
+    @Override
+    public T first() {
+        point = head;
+        if (head != null) {
+            return head.getValue();
+        }
+        return null;
+    }
+
+    @Override
+    public T last() {
+        point = tail;
+        if (tail != null) {
+            return tail.getValue();
+        }
+        return null;
+    }
+
+    @Override
+    public void next() {
+        point = point.getNextNode();
+    }
+
+    @Override
+    public void prev() {
+        if (point != head) {
+            if (point == null)
+                point = tail;
+            else
+                point = point.getPrevNode();
+        }
+    }
+}
---
+++ src/main/java/dsa/dataStructures/list/SinglyLinkedList.java
@@ -0,0 +1,105 @@
+package dsa.dataStructures.list;
+
+public class SinglyLinkedList<T> implements List<T> {
+    ListNode<T> head;
+    ListNode<T> tail;
+    ListNode<T> point;
+    int len;
+
+    @Override
+    public int length() {
+        return len;
+    }
+
+    @Override
+    public boolean empty() {
+        return head == null;
+    }
+
+    @Override
+    public T read() {
+        if (point != null) {
+            return point.getValue();
+        }
+        return null;
+    }
+
+    @Override
+    public void insert(T t) {
+        ListNode<T> help = new ListNode<>();
+        if (point == null) {
+            help.setValue(t);
+            if (tail == null) {
+                head = help;
+            } else {
+                tail.setNextNode(help);
+            }
+            tail = help;
+        } else {
+            help.setValue(point.getValue());
+            help.setNextNode(point.getNextNode());
+            point.setNextNode(help);
+            point.setValue(t);
+            point = help;
+        }
+        len++;
+    }
+
+    @Override
+    public void delete() {
+        ListNode<T> help;
+        if (point != null) {
+            if (point == head) {
+                head = tail = point = null;
+            } else if (point.getNextNode() == null) {
+                help = head;
+                while (help.getNextNode() != point)
+                    help = help.getNextNode();
+                help.setNextNode(null);
+                point = null;
+                tail = help;
+            } else {
+                help = point.getNextNode();
+                point.setNextNode(help.getNextNode());
+                point.setValue(help.getValue());
+                if (help == tail)
+                    tail = point;
+            }
+            len--;
+        }
+    }
+
+    @Override
+    public T first() {
+        point = head;
+        if (head != null) {
+            return head.getValue();
+        }
+        return null;
+    }
+
+    @Override
+    public T last() {
+        point = tail;
+        if (tail != null) {
+            return tail.getValue();
+        }
+        return null;
+    }
+
+    @Override
+    public void next() {
+        point = point.getNextNode();
+    }
+
+    @Override
+    public void prev() {
+        ListNode<T> help;
+        if (point != head) {
+            help = head;
+            while (help.getNextNode() != point)
+                help = help.getNextNode();
+            point = help;
+        }
+    }
+}
