f0ca69031ca0df6a667c306309ffd4d624295b18
Jakub Janak <janakja5@fel.cvut.cz>
2025-04-28 20:21:08 +0200

    added comments into the AVL tree

--- src/main/java/dsa/dataStructures/tree/balancedTree/AVLTree.java
+++ src/main/java/dsa/dataStructures/tree/balancedTree/AVLTree.java
@@ -2,6 +2,26 @@ package dsa.dataStructures.tree.balancedTree;
 
 import dsa.dataStructures.tree.Node;
 
+/**
+ * Implementation of an AVL Tree, a type of self-balancing binary search tree.
+ * <p>
+ * An AVL Tree maintains the property that for every node, the heights of the left and right subtrees differ by at most one.
+ * When an insertion or deletion causes this balance property to be violated, rotations are performed to restore balance.
+ * <p>
+ * The tree supports automatic balancing via single or double rotations based on the imbalance type:
+ * <table border="1">
+ *   <caption>Rotation Cases</caption>
+ *   <tr><th>Imbalance Case</th><th>Rotation Needed</th><th>Description</th></tr>
+ *   <tr><td>Left-Left (LL)</td><td>Single Right Rotation</td><td>New node inserted into left subtree of left child.</td></tr>
+ *   <tr><td>Left-Right (LR)</td><td>Left-Right Double Rotation</td><td>New node inserted into right subtree of left child.</td></tr>
+ *   <tr><td>Right-Right (RR)</td><td>Single Left Rotation</td><td>New node inserted into right subtree of right child.</td></tr>
+ *   <tr><td>Right-Left (RL)</td><td>Right-Left Double Rotation</td><td>New node inserted into left subtree of right child.</td></tr>
+ * </table>
+ * <p>
+ * This implementation assumes that elements stored in the tree are {@link Comparable} so they can be ordered.
+ *
+ * @param <T> the type of elements stored in the tree; must be comparable to itself
+ */
 public class AVLTree<T extends Comparable<T>> extends BalancedTree<T> {
     @Override
     Node<T> leftRotation(Node<T> subtreeRoot) {
@@ -105,6 +125,24 @@ public class AVLTree<T extends Comparable<T>> extends BalancedTree<T> {
         return height(node.getLeft()) - height(node.getRight());
     }
 
+    /**
+     * Balances the subtree rooted at the given node if it violates the AVL balance condition.
+     * <p>
+     * This method first updates the height of the given node and then checks its balance factor.
+     * Based on the balance factor and the balance of its child nodes, it determines which rotation
+     * (single or double) is necessary to restore the AVL property.
+     * <p>
+     * The method chooses the appropriate rotation according to these rules:
+     * <ul>
+     *   <li><b>Left-Left (LL) case</b>: Perform a single right rotation.</li>
+     *   <li><b>Left-Right (LR) case</b>: Perform a left rotation on the left child, then a right rotation.</li>
+     *   <li><b>Right-Right (RR) case</b>: Perform a single left rotation.</li>
+     *   <li><b>Right-Left (RL) case</b>: Perform a right rotation on the right child, then a left rotation.</li>
+     * </ul>
+     *
+     * @param node the root of the subtree to balance; may be {@code null}
+     * @return the new root of the balanced subtree (after any necessary rotations)
+     */
     private Node<T> balanceGraph(Node<T> node) {
         updateHeightForNode(node);
 
