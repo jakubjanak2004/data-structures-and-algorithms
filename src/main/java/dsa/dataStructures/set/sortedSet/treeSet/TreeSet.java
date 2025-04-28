2965239e153ef2c6ab8f71412d48853c1256e9f3
Jakub Janak <janakja5@fel.cvut.cz>
2025-04-28 18:15:30 +0200

    I have added templates for other algos and data structures to be implemented. I implemented a functional AVL tree(not optimised)

--- src/main/java/dsa/dataStructures/set/sortedSet/treeSet/TreeSet.java
+++ src/main/java/dsa/dataStructures/set/sortedSet/treeSet/TreeSet.java
@@ -1,157 +1,64 @@
 package dsa.dataStructures.set.sortedSet.treeSet;
 
 import dsa.dataStructures.set.sortedSet.SortedSet;
+import dsa.dataStructures.tree.Tree;
+import dsa.dataStructures.tree.UnbalancedBSTree;
+import dsa.dataStructures.tree.balancedTree.AVLTree;
+import org.jetbrains.annotations.NotNull;
 
-public class TreeSet<T extends Comparable<T>> extends SortedSet<T> {
-    private Node<T> root;
-    private int size;
+import java.util.Iterator;
+
+public class TreeSet<T extends Comparable<T>> extends SortedSet<T>{
+    private final Tree<T> tree = new AVLTree<>();
+
+    // todo injection of Tree class will be implemented
+//    public TreeSet(Tree<T> tree) {
+//        this.tree = tree;
+//    }
+
+    @Override
+    public T min() {
+        return tree.min();
+    }
+
+    @Override
+    public T max() {
+        return tree.max();
+    }
 
     @Override
     public int size() {
-        return size;
+        return tree.size();
     }
 
     @Override
     public T get(T element) {
-        Node<T> found = getNode(root, element);
-        return found == null ? null : found.getValue();
-    }
-
-    private Node<T> getNode(Node<T> node, T element) {
-        if (node == null) return null;
-
-        int comparison = element.compareTo(node.getValue());
-        if (comparison == 0) return node;
-        if (comparison < 0) return getNode(node.getLeft(), element);
-        return getNode(node.getRight(), element);
+        return tree.get(element);
     }
 
     @Override
     public void add(T element) {
-        if (root == null) {
-            root = new Node<>(element);
-            size++;
-            return;
-        }
-        addRecursive(root, element);
-    }
-
-    /**
-     * Helper method for recursively inserting an element into the tree.
-     *
-     * @param node    the current node
-     * @param element the element to insert
-     */
-    private void addRecursive(Node<T> node, T element) {
-        int comparison = element.compareTo(node.getValue());
-        if (comparison < 0) {
-            if (node.getLeft() == null) {
-                node.setLeft(new Node<>(element));
-                size++;
-            } else {
-                addRecursive(node.getLeft(), element);
-            }
-        }
-        if (comparison > 0) {
-            if (node.getRight() == null) {
-                node.setRight(new Node<>(element));
-                size++;
-            } else {
-                addRecursive(node.getRight(), element);
-            }
-        }
+        tree.add(element);
     }
 
     @Override
     public void addOrReplace(T element) {
-        if (root == null) {
-            root = new Node<>(element);
-            size++;
-            return;
-        }
-        addOrReplaceRecursive(root, element);
-    }
-
-    /**
-     * Helper method for recursively adding or replacing an element in the tree.
-     *
-     * @param node    the current node
-     * @param element the element to add or replace
-     */
-    private void addOrReplaceRecursive(Node<T> node, T element) {
-        int comparison = element.compareTo(node.getValue());
-        if (comparison == 0) {
-            node.setValue(element);
-            return;
-        }
-        if (comparison < 0) {
-            if (node.getLeft() == null) {
-                node.setLeft(new Node<>(element));
-                size++;
-            } else {
-                addOrReplaceRecursive(node.getLeft(), element);
-            }
-        }
-        if (comparison > 0) {
-            if (node.getRight() == null) {
-                node.setRight(new Node<>(element));
-                size++;
-            } else {
-                addOrReplaceRecursive(node.getRight(), element);
-            }
-        }
+        tree.addOrReplace(element);
     }
 
     @Override
     public boolean contains(T element) {
-        return getNode(root, element) != null;
+        return tree.contains(element);
     }
 
     @Override
     public void remove(T element) {
-        root = removeRecursive(root, element);
+        tree.remove(element);
     }
 
-    /**
-     * Helper method for recursively removing an element from the tree.
-     *
-     * @param node    the current node
-     * @param element the element to remove
-     * @return the new root of the subtree
-     */
-    private Node<T> removeRecursive(Node<T> node, T element) {
-        if (node == null) return null;
-
-        int comparison = element.compareTo(node.getValue());
-
-        if (comparison < 0) {
-            node.setLeft(removeRecursive(node.getLeft(), element));
-        } else if (comparison > 0) {
-            node.setRight(removeRecursive(node.getRight(), element));
-        } else {
-            size--;
-            // node to delete found
-            if (node.getLeft() == null) return node.getRight();
-            if (node.getRight() == null) return node.getLeft();
-
-            // node with two children: get smallest from right subtree
-            Node<T> minRight = findMin(node.getRight());
-            node.setValue(minRight.getValue());
-            node.setRight(removeRecursive(node.getRight(), minRight.getValue()));
-        }
-        return node;
-    }
-
-    /**
-     * Finds the node with the smallest value in a subtree.
-     *
-     * @param node the root of the subtree
-     * @return the node with the smallest value
-     */
-    private Node<T> findMin(Node<T> node) {
-        while (node.getLeft() != null) {
-            node = node.getLeft();
-        }
-        return node;
+    @NotNull
+    @Override
+    public Iterator<T> iterator() {
+        return tree.iterator();
     }
 }
