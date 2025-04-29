670db6c006eb9582941d310d173b2f9157538dba
Jakub Janak <janakja5@fel.cvut.cz>
2025-04-29 13:59:59 +0200

    search algorithms added

--- src/main/java/dsa/algorithms/search/InterpolationSearch.java
+++ src/main/java/dsa/algorithms/search/InterpolationSearch.java
@@ -4,7 +4,7 @@ public class InterpolationSearch {
     public static <T extends Number> int interpolationSearch(T[] array, T element, int first, int last) {
         // element not present
         if (first > last) return -1;
-        int mid = (int) (first + (element.doubleValue() - array[first].doubleValue()) * (last - first) / (array[last].doubleValue() - array[first].doubleValue()));
+        int mid = (int) Math.round((first + (element.doubleValue() - array[first].doubleValue()) * (last - first) / (array[last].doubleValue() - array[first].doubleValue())));
         // if mid out of range return not found
         if (mid < first || mid > last) return -1;
         if (array[mid].doubleValue() < element.doubleValue()) return interpolationSearch(array, element, mid + 1, last);
--- src/main/java/dsa/algorithms/search/SequentialSearch.java
+++ src/main/java/dsa/algorithms/search/SequentialSearch.java
@@ -12,18 +12,16 @@ public class SequentialSearch {
         return -1;
     }
 
-    // todo come back to this as this doesnt make sense for me
     public static <T> int sequentialSentinelSearch(T[] array, T element) {
         if (Objects.equals(element, array[array.length - 1])) {
             return array.length - 1;
         }
         array[array.length - 1] = element;
         int index = 0;
-        while (true) {
-            if (Objects.equals(element, array[index])) {
-                return index;
-            }
+        while (!Objects.equals(element, array[index])) {
             index++;
         }
+        if (index == array.length - 1) return -1;
+        return index;
     }
 }
