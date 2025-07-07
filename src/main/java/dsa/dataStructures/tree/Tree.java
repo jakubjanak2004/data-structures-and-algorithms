52114d99f93f82ee100795a7e4263a42ab2ee9dc
Jakub Janak <janakja5@fel.cvut.cz>
2025-07-07 21:10:19 +0200

    tree interface added

--- src/main/java/dsa/dataStructures/tree/Tree.java
+++ src/main/java/dsa/dataStructures/tree/Tree.java
@@ -1,15 +1,22 @@
 package dsa.dataStructures.tree;
 
-public interface Tree<T extends Comparable<T>> extends Iterable<T> {
+// todo we are using find in many method maybe abstract that and the sub methods will use predefined function performOnFound();
+public interface Tree<T extends Comparable<T>> extends Iterable<T>{
     int size();
+
     T get(T element);
+
     void add(T element);
+
+    // todo, the logic is the very similar to add, addOrReplace will define a function that gets called when the add finishes, it gets null or the found element whose value gets replaced for new one
     void addOrReplace(T element);
+
     boolean contains(T element);
+
     void remove(T element);
-    Node<T> treeMin(Node<T> node);
+
     T min();
+
     T max();
-    Node<T> treeMax(Node<T> node);
-    Node<T> treeSuccessor(Node<T> node);
 }
+
